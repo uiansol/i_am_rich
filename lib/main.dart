79d352a1111c674b213b6b7ade5373ebe2f5ca84
Uian Sol <usoldeveloper@gmail.com>
2022-07-15 19:58:14 -0300

    Add appbar and body to start as app design

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello world!"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
